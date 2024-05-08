4f05cd0a2a12b09205623244bfec268d88cff8ac
beacionca <beatricecionca@yahoo.com>
2024-05-08 11:21:07 +0300

    Added new form for autentification

--- Lucrare_Licenta/Form2.cs
+++ Lucrare_Licenta/Form2.cs
@@ -10,17 +10,11 @@ using System.Windows.Forms;
 
 namespace Lucrare_Licenta
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Form f = new Form2();
-            f.Show();
         }
     }
 }
